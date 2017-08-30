94f58a7ebc24d9435ffd6b2504e255485ee1e37c
alexyeet <31477677+alexyeet@users.noreply.github.com>
2017-08-30 09:13:12 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,22 @@
+#This program says hello and greets a person by name.
+#
+#alex fossum
+#August 25, 2017
+
+print ("hello")
+print ("what is your name?")
+name = input()
+print("it's nice to meet you, dab" + name + ".")
+
+print ("how old are you?")
+age = input()
+print ("that's cool. I am an unfeeling and immortal computer incapable of comprehending the concept of age or higher thought.")
+print ("how has your day been?")
+day=input()
+print ("what's your favorite subject in school?")
+subject= input()
+print ("i like " +subject+ " too, but my favorite is computer programming.")
+print ("i have to go. see you later!")
+bye= input()
+print ("bye!")
+
